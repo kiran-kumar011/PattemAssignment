8e1b79380c9a3cc4958e597d6ddce58592076a3a
kiran-kumar011 <kirankirankumar56@gmail.com>
2019-11-14 16:11:52 +0530

    WIP

---
+++ src/Components/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './Loader.css';
+
+const Loader = () => {
+	return(
+		<div className="lds-facebook">
+			<div>
+			</div>
+			<div>
+			</div>
+			<div>	
+			</div>
+		</div>
+	)
+}
+
+export default Loader;
